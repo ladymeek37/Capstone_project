1b9abecb0c220dcdedc933d68a5cf501977ffe09
Lady Meek <ladyjmeek@gmail.com>
2023-03-23 17:07:34 +0000

    updated serializers.py for users

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -15,6 +15,8 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 
         token["username"] = user.username
         token["first_name"] = user.first_name
+        token["last_name"] = user.last_name
+        token["home_park"] = user.home_park
 
         return token
 
@@ -31,7 +33,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
         # If added new columns through the User model, add them in the fields
         # list as seen below
         fields = ('username', 'password', 'email',
-                  'first_name', 'last_name',)
+                  'first_name', 'last_name', 'home_park')
 
     def create(self, validated_data):
 
@@ -40,6 +42,7 @@ class RegistrationSerializer(serializers.ModelSerializer):
             email=validated_data['email'],
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
+            home_park=validated_data['home_park']
 
             # If added new columns through the User model, add them in this
             # create method. Example below:
