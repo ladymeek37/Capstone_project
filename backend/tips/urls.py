6b70f6c1443f7ed12d51fa9f90bd4588de8c6421
Lady Meek <ladyjmeek@gmail.com>
2023-04-09 14:56:54 +0200

    added api request in tips view file to post new tip

--- backend/tips/urls.py
+++ backend/tips/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.tips_list),
+    path("all/", views.tips_list),
+    path("add/", views.post_tip),
 ]
--- backend/tips/views.py
+++ backend/tips/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .models import Tip
@@ -6,6 +8,7 @@ from .serializers import TipSerializer
 # Create your views here.
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def tips_list(request):
     tips = Tip.objects.all()
 
@@ -14,3 +17,15 @@ def tips_list(request):
 
     
     return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes({IsAuthenticated})
+def post_tip(request):
+    print(
+        'User ', f"{request.user.id} {request.user.email} {request.user.username}")
+    serializer = TipSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
