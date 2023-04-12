36e339c5385117b3c0c5622ea2c0812c73cac18a
Lady Meek <ladyjmeek@gmail.com>
2023-04-12 15:11:41 -0700

    created api call in favorite tips for user to add tip to favorites

--- backend/favorite_tips/urls.py
+++ backend/favorite_tips/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.user_favorited_tips)
+    path("", views.user_favorited_tips),
+    path("addfavorite/",views.add_to_favorites)
 ]
--- backend/favorite_tips/views.py
+++ backend/favorite_tips/views.py
@@ -4,12 +4,24 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .models import FavoriteTip
 from .serializers import FavoriteTipSerializer
+from rest_framework import status
 # Create your views here.
 
-
+#request to view favorited tips by user
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def user_favorited_tips(request):
     favorite_tips = FavoriteTip.objects.filter(user_id=request.user.id)
     serializer = FavoriteTipSerializer(favorite_tips, many=True)
     return Response(serializer.data)
+
+
+#request to add a tip to favorites
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def add_to_favorites(request):
+    serializer = FavoriteTipSerializer(data = request.data)
+    if serializer.is_valid():
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
